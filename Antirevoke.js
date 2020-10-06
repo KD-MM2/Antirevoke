0b55614f7bd0b63a2c079ca203cae1beba39135d
KD-MM2 <kaotd.z3@gmail.com>
2020-10-07 00:46:31 +0900

    update

--- Antirevoke.js
+++ Antirevoke.js
@@ -1,1 +1,20 @@
-var domains={"ocsp.int-x3.letsencrypt.org":1,"ocsp.apple.com":1,"gdmf.apple.com":1,"mesu.apple.com":1,"swscan.apple.com":1,"appldnld.apple.com":1,"world-gen.g.aaplimg.com":1};var proxy="PROXY 127.0.0.1:1080;";var direct="DIRECT;";function FindProxyForURL(url,host){var lastPos;do{if(domains.hasOwnProperty(host)){return proxy}lastPos=host.indexOf('.')+1;host=host.slice(lastPos)}while(lastPos>=1);return direct}
+var DIRECT = "DIRECT";
+var PROXY = "PROXY 127.0.0.1:8021";
+
+var antirevoke = ["ppq",
+		"mesu",
+		"ocsp",
+		"world-gen"
+		"gdmf"];
+
+function FindProxyForURL(url, host) {
+  host = host.toLowerCase();
+  for(var i = 0; i < antirevoke.length; i++){
+    var copyright = antirevoke[i];
+    if(shExpMatch(host, "*" + copyright + "*")){
+      return PROXY;
+    }
+  }
+
+  return DIRECT;
+}
